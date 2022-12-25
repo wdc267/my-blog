47e82ffe8615c1aa98fe2d588e45474ef3323368
traveller327 <1599358835@qq.com>
2022-12-25 22:59:47 +0800

    second commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const routes = [
+    {
+        name: '首页',
+        path: '/index',
+        component: ()=> import('../views/Index.vue'),
+    }
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+})
+
+export default router
---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  // 后端配置
+  // server: {
+  //   hmr: true,
+  //   port: 3001,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8081/',
+  //       secure: false,
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/api': '/api',
+  //       },
+  //     }
+  //   }
+  // },
+  
+  resolve: {
+    alias: {
+      // 配置路径别名
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+})
